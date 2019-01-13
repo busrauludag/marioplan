ede5db302683d1771a67cff7863294cbdf242187
busrauludag <busrauludag13@gmail.com>
2019-01-13 20:24:58 +0300

    navbar component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+// Components
+import Navbar from './components/layout/Navbar';
+
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          MarioPlan
+          <Navbar />
         </div>
       </BrowserRouter>
     );
