9df7bc43652bbd3e61cb1df421e628a99eed251b
busrauludag <busrauludag13@gmail.com>
2019-01-13 19:33:17 +0300

    react router dom installed and import to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        MarioPlan
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          MarioPlan
+        </div>
+      </BrowserRouter>
     );
   }
 }
