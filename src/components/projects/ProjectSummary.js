9bb09ee21587e066d98dff82b77a37036d4c044c
busrauludag <busrauludag13@gmail.com>
2019-01-13 21:54:31 +0300

    project summary component for use in project lit item

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+  return(
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Project Title</span>
+        <p>Posted by Büşra ULUDAĞ</p>
+        <p className="grey-text">10 Ocak 2019, 13:00</p>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectSummary;
