deb74fd6b71f7f03f12eaab6de3740f2a83453d6
busrauludag <busrauludag13@gmail.com>
2019-01-13 21:07:55 +0300

    dashboard, project list and notifications components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 // Components
 import Navbar from './components/layout/Navbar';
-
+import Dashboard from './components/dashboard/Dashboard';
 
 class App extends Component {
   render() {
@@ -11,6 +11,9 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar />
+          <Switch>
+            <Route path="/" component={ Dashboard } />
+          </Switch>
         </div>
       </BrowserRouter>
     );
---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ProjectList = () => {
+  return(
+    <div className="project-list section">
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Büşra ULUDAĞ</p>
+          <p className="grey-text">10 Ocak 2019, 13:00</p>
+        </div>
+      </div>
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Büşra ULUDAĞ</p>
+          <p className="grey-text">10 Ocak 2019, 13:00</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectList;
