2db93d44ded493638b6536be61bfb297d9c22404
busrauludag <busrauludag13@gmail.com>
2019-01-16 23:44:06 +0300

    create project store and get store datas to dashboard with props

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,14 +3,17 @@
 import React, { Component } from 'react';
 import Notifications from './Notifications';
 import ProjectList from '../projects/ProjectList';
+import { connect } from 'react-redux'; // to connect to store
 
 class Dashboard extends Component{
   render(){
+    // console.log(this.props);
+    const { projects } = this.props;
     return(
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-            <ProjectList />
+            <ProjectList projects={projects} />
           </div>
           <div className="col s12 m5 offset-m1">
             <Notifications />
@@ -21,4 +24,10 @@ class Dashboard extends Component{
   }
 }
 
-export default Dashboard;
+const matStateToProps = (state) => {
+  return{
+    projects: state.project.allProjects
+  }
+}
+
+export default connect(matStateToProps)(Dashboard);  // to know connect which store data and mapping props
