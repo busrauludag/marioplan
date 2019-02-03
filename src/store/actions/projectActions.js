9e2d3cceb804f6ef3a1b457b8c65847950605df9
busrauludag <busrauludag13@gmail.com>
2019-02-03 15:28:21 +0300

    created project and save to database

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,17 @@
 export const createProject = (project) =>{
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make async call to database
-    dispatch({ type: 'CREATE_PROJECT', project: project });
+    const firestore = getFirestore();
+    firestore.collection('projects').add({
+      ...project,
+      authorFirstName: 'Busra',
+      authorLastName: 'Uludag',
+      authorId: 12345,
+      createdAt: new Date()
+    }).then(() => {
+      dispatch({ type: 'CREATE_PROJECT', project }); // run action
+    }).catch((err) => {
+      dispatch({ type: 'CREATE_PROJECT_ERROS', err });
+    });
   }
 };
